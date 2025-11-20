da0eead86087c51e2c9bd7ea26fdd4c127e02f82
Samuel Laurindo <samuellaurindodelima2004@gmail.com>
2025-11-20 11:57:28 -0300

    feat: add get poll feature

---
+++ src/main/java/com/smlaurindo/realtime_polls/dto/GetPollResponse.java
@@ -0,0 +1,20 @@
+package com.smlaurindo.realtime_polls.dto;
+
+import com.smlaurindo.realtime_polls.domain.PollStatus;
+
+import java.util.List;
+
+public record GetPollResponse(
+        String id,
+        String question,
+        PollStatus status,
+        String startsAt,
+        String endsAt,
+        List<OptionResponse> options
+) {
+    public record OptionResponse(
+            String id,
+            String text,
+            int votes
+    ) {}
+}
